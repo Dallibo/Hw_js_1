be4e6481e9982338418dc1e7bc46218f013f01eb
Dallibo <denismalyarenko1@gmail.com>
2023-05-31 00:12:59 +0300

    HW

--- script1.js
+++ script1.js
@@ -1,34 +1,34 @@
-let num = +prompt("Введите число:");
+let num = +prompt("Введите число что бы возвести в 2 степень:");
 let average = (num**2);
-alert("Вторая степень: " + average);
+alert("Число^2: " + average);
 
-let num1 = +prompt("Введите первое число:");
-let num2 = +prompt("Введите второе число:");
+let num1 = +prompt("Введите первое число для вычисления среднего арефметического:");
+let num2 = +prompt("Введите второе число для вычисления среднего арефметического:");
 let average1 = (num1 + num2) / 2;
-alert("Середнє арифметичне: " + average1);
+alert("Среднее арефметическое: " + average1);
 
-let num3 = +prompt("Введите длинну стороны");
+let num3 = +prompt("Введите длинну стороны чтобы узнать площадь квадрата");
 let average2 = (num3*num3);
 alert("Площадь квадрата: " + average2);
 
-let num4 = +prompt("Введите километры");
+let num4 = +prompt("Введите километры чтобы перевести в мили");
 const mile = 0.621371
 let average3 = (num4*mile);
 alert("Мили: " + average3);
 
-let num5 = +prompt("Введите первое число:");
-let num6 = +prompt("Введите второе число:");
+let num5 = +prompt("Введите первое число чтобы узнать сумму, разницу, произведение, частное:");
+let num6 = +prompt("Введите второе число чтобы узнать сумму, разницу, произведение, частное:");
 let sum = num5 + num6;
 let difference = num5 - num6;
 let product = num5 * num6;
 let quotient = num5 / num6;
 alert("Сумма: " + sum);
 alert("Разница: " + difference);
-alert("Умножение: " + product);
-alert("Деление: " + quotient);
+alert("Произведение: " + product);
+alert("Частное: " + quotient);
 
-let a = +prompt("Введите значение a:");
-let b = +prompt("Введите значение b:");
+let a = +prompt("Введите значение a для вычисления x:");
+let b = +prompt("Введите значение b для вычисления x:");
 let x = -b / a;
 alert("Значение x: " + x);
 
